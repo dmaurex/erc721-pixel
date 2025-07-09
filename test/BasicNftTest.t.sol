e071427f7ab6cc971dbc1fa84aff8937697e04b0
dmaurex <dmaurex@gmail.com>
2025-07-09 15:58:58 +0200

    Implement and test basic NFT

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.30;
+
+import {Script} from "forge-std/Script.sol";
+import {DeployBasicNft} from "script/DeployBasicNft.s.sol";
+import {BasicNft} from "src/BasicNft.sol";
+
+contract BasicNftTest is Script {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+    address public COLLECTOR = makeAddr("collector");
+    string public constant PIXEL = "ipfs://example";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Pixel";
+        string memory actualName = basicNft.name();
+        // Strings cannot be compared directly -> compare their hashes
+        assert(keccak256(abi.encode(expectedName)) == keccak256(abi.encode(actualName)));
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(COLLECTOR);
+        basicNft.mintNft(PIXEL);
+
+        assert(keccak256(abi.encodePacked(basicNft.tokenURI(0))) == keccak256(abi.encodePacked(PIXEL)));
+    }
+}
